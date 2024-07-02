87c9aa99b46cd5f9488e6159dd20dcad264711be
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-07-02 14:29:52 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//*****************************************************************************
+//** 231. Power of Two leetcode                                              **
+//*****************************************************************************
+
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0) return false;
+    return (n & (n - 1)) == 0;
+/*
+//**************************************************************
+//** This was my first attempt, it ran slower                 **
+//** The above code runs faster  -Dan                         **
+//**************************************************************
+
+    if(n <= 0) return false;
+    if(n % 2 == 1 && n > 1) return false;
+    double t = 2;
+    for(int i = 0; i < n+1; i++)
+    {
+        double out = pow(t, i);
+//        printf("Out = %f\n",out);
+        if(out == n) return true;
+        if(i > n) return false;
+    }
+    return false;
+*/
+}
